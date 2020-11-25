ed14761a5c7948ca5b3558e4c9faf55e13ef04b3
Quant-Kor <53992007+Quant-Kor@users.noreply.github.com>
2020-11-25 22:57:40 +0900

    re chap1

--- Cppstudy/restart.cpp
+++ Cppstudy/restart.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main(void)
 {
-	int val1;
-	std::cout << "첫 번째 숫자 입력: ";
-	std::cin >> val1;
-
-	int val2;
-	std::cout << "두 번째 숫자 입력: ";
-	std::cin >> val2;
-
-	int result = val1 + val2;
-	std::cout << "덧셈 결과: " << result << std::endl;
+	int num = 20;
+	cout << "Hello World!" << endl;
+	cout << "Hello " << "World!" << endl;
+	cout << num << ' ' << 'A';
+	cout << ' ' << 3.14 << endl;
 	return 0;
 }
