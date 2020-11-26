820acfaa5fc62d6a603c80c1721f3f41d2aa9fa3
Quant-Kor <53992007+Quant-Kor@users.noreply.github.com>
2020-11-26 15:12:18 +0900

    chp2

--- Cppstudy/restart.cpp
+++ Cppstudy/restart.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 
+void SwapByRef2(int& ref1, int& ref2)
+{
+	int temp = ref1;
+	ref1 = ref2;
+	ref2 = temp;
+}
+
 int main(void)
 {
-	int num = 20;
-	cout << "Hello World!" << endl;
-	cout << "Hello " << "World!" << endl;
-	cout << num << ' ' << 'A';
-	cout << ' ' << 3.14 << endl;
+	int val1 = 10;
+	int val2 = 20;
+
+	SwapByRef2(val1, val2);
+	cout << "val1: " << val1 << endl;
+	cout << "val2: " << val2 << endl;
 	return 0;
 }
