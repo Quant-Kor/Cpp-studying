35ad900077c5286b8f66250cc83a04e0f9310057
Quant-Kor <53992007+Quant-Kor@users.noreply.github.com>
2020-12-27 15:03:54 +0900

    Chp-3-1

--- Cppstudy/restart.cpp
+++ Cppstudy/restart.cpp
@@ -1,26 +1,66 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
 
-class Simple
+#define ID_LEN 20
+#define MAX_SPD 200
+#define FUEL_STEP 2
+#define ACC_STEP 10
+#define BRK_STEP 10
+
+struct Car
 {
-public:
-	Simple()
-	{
-		cout << "I'm simple constructor!" << endl;
-	}
+	char gamerID[ID_LEN]; // 소유자 ID
+	int fuelGauge; // 연료량
+	int curSpeed;
 };
+
+void ShowCarState(const Car& car)
+{
+	cout << "소유자ID: " << car.gamerID << endl;
+	cout << "연료량: " << car.fuelGauge << endl;
+	cout << "현재속도: " << car.curSpeed << "km/s" << endl << endl;
+}
+
+void Accel(Car& car)
+{
+	if (car.fuelGauge <= 0)
+	{
+		return;
+	}
+	else
+		car.fuelGauge -= FUEL_STEP;
+
+	if (car.curSpeed + ACC_STEP >= MAX_SPD)
+	{
+		car.curSpeed = MAX_SPD;
+		return;
+	}
+
+	car.curSpeed += ACC_STEP;
+}
+
+void Break(Car& car)
+{
+	if (car.curSpeed < BRK_STEP)
+	{
+		car.curSpeed = 0;
+		return;
+	}
+	car.curSpeed = BRK_STEP;
+}
 
 int main(void)
 {
-	cout << "case 1: ";
-	Simple* sp1 = new Simple;
+	Car run99 = { "run99", 100, 0 };
+	Accel(run99);
+	Accel(run99);
+	ShowCarState(run99);
+	Break(run99);
+	ShowCarState(run99);
 
-	cout << "case 2: ";
-	Simple* sp2 = (Simple*)malloc(sizeof(Simple) * 1);
-
-	cout << endl << "end of main" << endl;
-	delete sp1;
-	free(sp2);
+	Car sped77 = { "sped77", 100, 0 };
+	Accel(sped77);
+	Break(sped77);
+	ShowCarState(sped77);
 	return 0;
 }
