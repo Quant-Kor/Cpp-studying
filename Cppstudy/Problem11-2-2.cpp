f63a3dbf3f71826369d3bba60a815e6afcd06a4b
hustle-dev <dlwoabsdk@gmail.com>
2021-01-15 15:09:35 +0900

    chapter10

--- Cppstudy/Problem11-2-2.cpp
+++ Cppstudy/Problem11-2-2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+
 
 class BoundCheckIntArray
 {
@@ -8,7 +10,6 @@ private:
 	int arrlen;
 	BoundCheckIntArray(const BoundCheckIntArray& arr) { }
 	BoundCheckIntArray& operator=(const BoundCheckIntArray& arr) { }
-
 public:
 	BoundCheckIntArray(int len) : arrlen(len)
 	{
@@ -33,48 +34,43 @@ public:
 		return arr[idx];
 	}
 	int GetArrLen() const { return arrlen; }
-	~BoundCheckIntArray() { delete[] arr; }
+	~BoundCheckIntArray()
+	{
+		delete[] arr;
+	}
 };
 
-typedef BoundCheckIntArray* BoundCheckIntArrayPtr;
+typedef BoundCheckIntArray* BoundCheckIntPtrArray;
 
 class BoundCheck2DIntArray
 {
 private:
-	BoundCheckIntArrayPtr** arr;
 	int arrlen;
+	BoundCheckIntArray** arr;
 	BoundCheck2DIntArray(const BoundCheck2DIntArray& arr) { }
-	BoundCheck2DIntArray& operator=(const BoundCheck2DIntArray& arr) { }
+	BoundCheck2DIntArray& operator=(const BoundCheck2DIntArray& arr) {}
 public:
-	BoundCheck2DIntArray(int x = 0, int y = 0) : arrlenx(x), arrleny(y)
+	BoundCheck2DIntArray(int col, int row) : arrlen(col)
 	{
-		arr = new int*[arrlenx];
-		for (int i = 0; i < arrlenx; i++)
-		{
-			arr[i] = new int[arrleny];
-		}
+		arr = new BoundCheckIntPtrArray[col];
+		for (int i = 0; i < col; i++)
+			arr[i] = new BoundCheckIntArray(row);
 	}
-	int GetArrLenx() const { return arrlenx; }
-	int GetArrLeny() const { return arrleny; }
-
-	int& operator[](int idx)
+	BoundCheckIntArray& operator[] (int idx)
 	{
-		if (idx < 0 || idx >= arrlenx)
+		if (idx < 0 || idx >= arrlen)
 		{
 			cout << "Array index out of bound exception" << endl;
 			exit(1);
 		}
-		return arr[idx];
+		return *(arr[idx]);
 	}
-
 	~BoundCheck2DIntArray()
 	{
-		for (int i = 0; i < arrlenx; i++)
-			delete[] arr[i];
-
-		delete[] arr;
+		for (int i = 0; i < arrlen; i++)
+			delete arr[i];
+		delete arr;
 	}
-
 };
 
 int main(void)
@@ -84,13 +80,11 @@ int main(void)
 	for (int n = 0; n < 3; n++)
 		for (int m = 0; m < 4; m++)
 			arr2d[n][m] = n + m;
-
 	for (int n = 0; n < 3; n++)
 	{
 		for (int m = 0; m < 4; m++)
 			cout << arr2d[n][m] << ' ';
-		cout << endl;
+		cout << endl;	
 	}
-
 	return 0;
 }
